cf43ac65e04acec6b28e3bd1dd0d7e795327b715
yachna2507 <Singh.yachna25@gmail.com>
2026-06-13 13:02:03 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,18 +7,21 @@ function Projects() {
             <div className="project-card">
 
                 <h3>MERN Ecommerce Dashboard</h3>
+        <p className="tech-stack">React | Node.js | MongoDB | Express</p>
         
-        
+        <div className="project-links">
          <a
                 href="https://mern-ecommerce-dashboard-tau.vercel.app"
                 target="_blank"
                 rel="noreferrer"
                 >Live Demo</a>
+                    <a href="https://github.com/yachna2507/mern-ecommerce-dashboard" target="_blank">GitHub</a>
+                    </div>
                     
                 </div>
             <div className="project-card">
                 <h3> Shopify Admin Dashboard</h3>
-                    
+                    <div className="project-links">
                 <a
                 href="https://shopify-admin-dashboard-client.vercel.app"
                 target="_blank"
@@ -26,12 +29,14 @@ function Projects() {
                 >
                     Live Demo
                 </a>
+                      <a href="https://github.com/yachna2507/shopify-admin-dashboard" target="_blank">GitHub</a>
+                    </div>
                     
                     
             </div>
             <div className="project-card">
                 <h3>PHP CRUD Admin Panel</h3>
-                    
+                    <div className="project-links">
                 <a
                 href="http://yachnacrud.great-site.net/php_crud_project/login.php"
                 target="_blank"
@@ -40,6 +45,8 @@ function Projects() {
                 >
                     Live Demo
                 </a>
+                      <a href="https://yachnacrud.great-site.net/php_crud_project" target="_blank">GitHub</a>
+                    </div>
                     
             </div>
                     </div>
