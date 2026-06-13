957b771894a8027a22e55c497a3db7bc6ddaa386
yachna2507 <Singh.yachna25@gmail.com>
2026-06-13 12:08:43 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,16 @@ function Projects() {
         <h2>Projects</h2>
         <div className="projects-grid">
             
-            <div className="projects-card">
+            <div className="project-card">
                 <h3>MERN Ecommerce Dashboard</h3>
                 <a
                 href="https://mern-ecommerce-dashboard-tau.vercel.app"
                 target="_blank"
                 rel="noreferrer"
                 >Live Demo</a>
-            </div>
-            <div className="projects-card">
+            
+                    </div>
+            <div className="project-card">
                 <h3> Shopify Admin Dashboard</h3>
                 <a
                 href="https://shopify-admin-dashboard-client.vercel.app"
@@ -22,7 +23,7 @@ function Projects() {
                     Live Demo
                 </a>
             </div>
-            <div className="projects-card">
+            <div className="project-card">
                 <h3>PHP CRUD Admin Panel</h3>
                 <a
                 href="http://yachnacrud.great-site.net/php_crud_project/login.php"
